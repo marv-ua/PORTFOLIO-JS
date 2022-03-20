455354201862d7c5ed3c740860da4238716dd0ba
marv-ua <lg.marvel@gmail.com>
2022-03-20 13:12:21 +0200

    hw: add days to timer

--- js/timer.js
+++ js/timer.js
@@ -8,15 +8,17 @@ const updateClock = () => {
     const dateDeadline = new Date(deadline).getTime();
     const timeRemaining = (dateDeadline - date) / 1000; // разница в секундах
 
-    const hours = Math.floor(timeRemaining / 60 / 60);  // округлили в меньшую сторону до целого
+    const days = Math.floor(timeRemaining / 60 / 60 / 24);
+    const hours = Math.floor((timeRemaining / 60 / 60) % 24);  // округлили в меньшую сторону до целого
     const minutes = Math.floor((timeRemaining / 60) % 60);
     const seconds = Math.floor(timeRemaining % 60);
 
+    const fDays = days < 10 ? '0' + days : days;
     const fHours = hours < 10 ? '0' + hours : hours;
     const fMinutes = minutes < 10 ? '0' + minutes : minutes;
     const fSecons = seconds < 10 ? '0' + seconds : seconds;
     
-    timerBlock.textContent = `${fHours}:${fMinutes}:${fSecons}`;    
+    timerBlock.textContent = `${fDays}:${fHours}:${fMinutes}:${fSecons}`;    
 
     if (timeRemaining <= 0) {
         clearInterval(interval);
